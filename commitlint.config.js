46ce688c0df0e5defcbf088e26f781d22bbd5a2e
m10maeda <mio.maeda.kari@gmail.com>
2022-11-20 22:28:57 +0900

    chore: commitlint setup

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+};
+
+module.exports = config;
