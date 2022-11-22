b52014ded1a7080391ee14da67dc5ef3b858dc88
m10maeda <mio.maeda.kari@gmail.com>
2022-11-22 23:17:08 +0900

    chore(backend-feature-shared): make reports available for inspection

--- libs/backend/feature/shared/jest.config.ts
+++ libs/backend/feature/shared/jest.config.ts
@@ -12,4 +12,15 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/libs/backend/feature/shared',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'backend shared feature unit tests',
+        outputDirectory: 'reports/libs/backend/feature/shared',
+        outputName: 'unit-test-results.xml',
+      },
+    ],
+  ],
 };
