1c8b9118ef83aef1a54e4910a5fedb7a24600050
m10maeda <mio.maeda.kari@gmail.com>
2022-11-22 23:16:38 +0900

    chore(backend-feature-shared): initialize library

---
+++ libs/backend/feature/shared/src/lib/BackendFeatureShared.spec.ts
@@ -0,0 +1,7 @@
+import { backendFeatureShared } from './BackendFeatureShared';
+
+describe('backendFeatureShared', () => {
+  it('should work', () => {
+    expect(backendFeatureShared()).toEqual('backend-feature-shared');
+  });
+});
