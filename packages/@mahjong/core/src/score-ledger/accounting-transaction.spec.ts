47a2ac10723e248b7ab0c4d645b720023b0c026b
Mio Maeda <mio.maeda.kari@gmail.com>
2026-04-25 22:00:19 +0900

    test(core): テストケースの追加

--- packages/@mahjong/core/src/score-ledger/accounting-transaction.spec.ts
+++ packages/@mahjong/core/src/score-ledger/accounting-transaction.spec.ts
@@ -22,5 +22,37 @@ describe('AccountingTransaction', () => {
         );
       }).toThrow(Error);
     });
+
+    it('amount が 0 の AccountingEntry のみを渡した場合、エラーを投げること', () => {
+      expect(() => {
+        new AccountingTransaction(
+          [
+            new AccountingEntry(SeatAccountOwner.East, new Point(0)),
+            new AccountingEntry(SeatAccountOwner.South, new Point(0)),
+          ],
+          new Round(
+            new RoundProgress(RoundWind.East, new RoundIndex(1)),
+            new Honba(0),
+          ),
+        );
+      }).toThrow(Error);
+    });
+  });
+
+  describe('createTransferTransaction static メソッド', () => {
+    it('amount の合計が 0 でない場合、エラーを投げること', () => {
+      expect(() => {
+        AccountingTransaction.createTransferTransaction(
+          [
+            [SeatAccountOwner.East, new Point(1000)],
+            [SeatAccountOwner.South, new Point(-500)],
+          ],
+          new Round(
+            new RoundProgress(RoundWind.East, new RoundIndex(1)),
+            new Honba(0),
+          ),
+        );
+      }).toThrow(Error);
+    });
   });
 });
