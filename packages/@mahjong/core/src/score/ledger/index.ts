aedd9056e84a4a5c2544dc535f3182e607618a6c
Mio Maeda <mio.maeda.kari@gmail.com>
2026-05-09 16:25:48 +0900

    feat(core): 得点管理台帳の追加

--- packages/@mahjong/core/src/score/ledger/index.ts
+++ packages/@mahjong/core/src/score/ledger/index.ts
@@ -1,5 +1,12 @@
 export { ScoreLedgerEvent, ScoreTransacted } from './event';
 
+export { ScoreLedger } from './score-ledger';
+
+export { ScoreTransaction } from './score-transaction';
+
 export { ScoreEntry } from './score-entry';
 
 export { ScoreHolder, SeatScoreHolder, PotScoreHolder } from './score-holder';
+
+export { InvalidDuplicatedHolderError } from './invalid-duplicated-holder-error';
+export { InvalidNoZeroSumError } from './invalid-no-zero-sum-error';
---
+++ packages/@mahjong/core/src/score/ledger/invalid-duplicated-holder-error.ts
@@ -0,0 +1,1 @@
+export class InvalidDuplicatedHolderError extends Error {}
